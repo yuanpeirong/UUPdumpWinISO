d0a1f4038e60f54dff56f8e517008895e2908436
yuanpeirong <yuanpeirong@vip.qq.com>
2025-12-24 11:50:57 +0800

    Windows10_21H2_amd64_Feature_update19044.6693

--- Windows10_21H2_amd64_Feature_update/uup_download_linux.sh
+++ Windows10_21H2_amd64_Feature_update/uup_download_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Generated on 2025-12-20 03:05:25 GMT
+#Generated on 2025-12-24 03:45:45 GMT
 
 # Proxy configuration
 # If you need to configure a proxy to be able to connect to the internet,
@@ -47,7 +47,7 @@ fi
 
 echo ""
 echo "Retrieving aria2 script for the UUP set..."
-aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=5d19c7c8-5545-4c39-8a38-6b60154097c8&pack=zh-cn&edition=professional&aria2=2"
+aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=00faef79-4588-4ede-bd14-2e563d528628&pack=zh-cn&edition=professional&aria2=2"
 if [ $? != 0 ]; then
   echo "Failed to retrieve aria2 script"
   exit 1
